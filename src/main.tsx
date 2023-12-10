56f489dc8a324b0a1e4c19438ad3f89a838be302
Matheus Dario <dario.matheus97@gmail.com>
2023-12-10 00:19:13 -0300

    config themeProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+
 import Home from './templates/App/App.tsx';
+import { theme } from './styles/theme.ts';
 import { GlobalStyles } from './styles/global-styles.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Home />
-    <GlobalStyles />
+    <ThemeProvider theme={theme}>
+      <Home />
+      <GlobalStyles />
+    </ThemeProvider>
   </React.StrictMode>,
 );
--- src/styles/global-styles.ts
+++ src/styles/global-styles.ts
@@ -7,6 +7,6 @@ export const GlobalStyles = createGlobalStyle`
   }
 
   body {
-    background-color: skyblue;
+    background-color: ${({ theme }) => theme.colors.white};
   }
 `;
